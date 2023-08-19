87d8fd7120c95c7d387184e4d5dba70db1932247
Peterdinis <peterdinis0@pobox.sk>
2023-08-19 11:24:17 +0200

    feat: Fix bg image display

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 
 const LoginForm: FC = () => {
     return (
-        <>
-        
-        </>
-    )
+        <div className="relative h-full w-full bg-cover bg-center" style={{ backgroundImage: "url('/images/hero.jpg')" }}>
+            ggg
+        </div>
+    );
 }
 
 export default LoginForm;
