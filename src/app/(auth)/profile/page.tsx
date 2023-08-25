47c66ee1ef854129b8d8abf3936a7bb23cced890
Peterdinis <peterdinis0@pobox.sk>
2023-08-25 11:48:10 +0200

    feat: Start to rebuilding auth WIP

--- src/app/(auth)/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProfileWrapper from "@/components/auth/ProfileWrapper";
-import { NextPage } from "next";
-
-const ProfilePage: NextPage = () => {
-    return <ProfileWrapper />
-}
-
-export default ProfilePage;
--- src/app/(auth)/register/page.tsx
+++
@@ -1,8 +0,0 @@
-import RegisterForm from "@/components/auth/RegisterForm";
-import { NextPage } from "next";
-
-const RegisterPage: NextPage = () => {
-    return <RegisterForm />
-}
-
-export default RegisterPage;
