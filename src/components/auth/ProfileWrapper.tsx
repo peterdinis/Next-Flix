8845ae32d009f950f45a03d0da49e981e552d9ca
Peterdinis <peterdinis0@pobox.sk>
2023-08-26 15:13:15 +0200

    feat: Adding new images to public

--- src/components/auth/ProfileWrapper.tsx
+++ src/components/auth/ProfileWrapper.tsx
@@ -1,4 +1,9 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
+import { NextPageContext } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import * as api from "@/api/queries/userQueries";
+import {useQuery} from "@tanstack/react-query";
 
 const ProfileWrapper: FC = () => {
     return (
