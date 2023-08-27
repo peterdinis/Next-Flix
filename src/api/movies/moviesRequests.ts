517ab001ece9a3a8c7ec33dab04ce9d091991371
Peterdinis <peterdinis0@pobox.sk>
2023-08-27 16:55:44 +0200

    feat: Setupping tmdb

---
+++ src/api/movies/moviesRequests.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const useFetchTrendingRequest = () => {
+    
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import ErrorBoundary from "@/components/shared/GlobalError";
 import AuthContextProvider from "@/context/AuthContext";
+import { RecoilRoot } from "recoil";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,10 +24,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthContextProvider>
-          <ErrorBoundary>
-            {children}
-            <Toaster />
-          </ErrorBoundary>
+          <RecoilRoot>
+            <ErrorBoundary>
+              {children}
+              <Toaster />
+            </ErrorBoundary>
+          </RecoilRoot>
         </AuthContextProvider>
       </body>
     </html>
