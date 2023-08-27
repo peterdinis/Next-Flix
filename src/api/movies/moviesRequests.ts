545413f181d6a17d8510929979504bb3fe1910fa
Peterdinis <peterdinis0@pobox.sk>
2023-08-27 17:10:54 +0200

    feat: Writing movies api requests wip

--- src/api/movies/moviesRequests.ts
+++ src/api/movies/moviesRequests.ts
@@ -1,8 +1,36 @@
-import axios from "axios";
+import useSWR, {Fetcher} from "swr";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const fetcher: Fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-const BASE_URL = 'https://api.themoviedb.org/3';
 
-const useFetchTrendingRequest = () => {
-    
+export function useFetchTrendingMovies() {
+  const url = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`;
+
+  const { data, error } = useSWR(url, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading: !data && !error,
+  };
+}
+
+export function useFetchNetflixOriginals() {
+  const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`;
+  const { data, error } = useSWR(url, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading: !data && !error,
+  };
+}
+
+
+export function useFetchTopRated() {
+    const url = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`
+    const {data, error} = useSWR(url, fetcher);
 }
