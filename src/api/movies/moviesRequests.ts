671b8d010d847b95b23f89781fbecd43c3bc98e0
Peterdinis <peterdinis0@pobox.sk>
2023-08-28 17:01:26 +0200

    feat: Updating movies request with fallbackData

--- src/api/movies/moviesRequests.ts
+++ src/api/movies/moviesRequests.ts
@@ -2,6 +2,11 @@ import useSWR, { Fetcher } from "swr";
 
 export const baseUrl = "https://image.tmdb.org/t/p/original";
 
+const initialMovieData = {
+  name: "Custom Name",
+  description: "Custom Description",
+};
+
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const fetcher: Fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -10,7 +15,10 @@ const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 export function useFetchTopTrendingMovies() {
   const url = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -22,7 +30,10 @@ export function useFetchTopTrendingMovies() {
 export function useFetchTrendingMovies() {
   const url = `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`;
 
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -33,7 +44,10 @@ export function useFetchTrendingMovies() {
 
 export function useFetchNetflixOriginals() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -44,7 +58,10 @@ export function useFetchNetflixOriginals() {
 
 export function useFetchTopRated() {
   const url = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -56,7 +73,10 @@ export function useFetchTopRated() {
 export function useFetchTopActionMovies() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`;
 
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -67,7 +87,10 @@ export function useFetchTopActionMovies() {
 
 export function useFetchComedyMovies() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -78,7 +101,10 @@ export function useFetchComedyMovies() {
 
 export function useHororMovies() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -90,7 +116,10 @@ export function useHororMovies() {
 export function useFetchRomanceMovies() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`;
 
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
@@ -101,7 +130,10 @@ export function useFetchRomanceMovies() {
 
 export function useFetchDocumentaries() {
   const url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`;
-  const { data, error } = useSWR(url, fetcher);
+  const { data, error } = useSWR(url, fetcher, {
+    fallbackData: initialMovieData,
+    revalidateOnMount: true,
+  });
 
   return {
     data,
