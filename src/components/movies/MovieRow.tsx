38708dacff36d2bbb2cc7301cc8aed0abe87c4e3
Peterdinis <peterdinis0@pobox.sk>
2023-08-28 17:17:25 +0200

    feat: Adding moviewRow component

--- src/components/movies/MovieRow.tsx
+++ src/components/movies/MovieRow.tsx
@@ -2,7 +2,7 @@ import { FC, useState, useRef } from "react";
 import { DocumentData } from "firebase/firestore";
 import { Movie } from "@/types/moviesTypes";
 import Thumbnail from "./Thumbnail";
-import {FiArrowLeft, FiArrowRight} from "react-icons/fi";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 interface IMovieRowProps {
   title: string;
@@ -27,37 +27,39 @@ const MovieRow: FC<IMovieRowProps> = ({ title, movies }: IMovieRowProps) => {
       rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
     }
   };
-  
+
+  console.info("Movies", movies);
+
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
-    <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
-      {title}
-    </h2>
-    <div className="group relative md:-ml-2">
-      <FiArrowLeft
-        className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
-          !isMoved && 'hidden'
-        }`}
-        onClick={() => handleClick('left')}
-      />
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <FiArrowLeft
+          className={`absolute  top-0 text-blue-50 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && "hidden"
+          }`}
+          onClick={() => handleClick("left")}
+        />
 
-      <div
-        ref={rowRef}
-        className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
-      > 
-      {/* TODO: Fix typing here */}
-        {movies.results && movies.results.map((movie: {id: any}) => (
-          <Thumbnail key={movie.id} movie={movie} />
-        ))}
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+        >
+          {movies.results &&
+            movies.results.map((movie: { id: string | number }) => (
+              <Thumbnail key={movie.id} movie={movie} />
+            ))}
+        </div>
+
+        <FiArrowRight
+          className={`absolute top-0 bottom-0 text-blue-50 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          onClick={() => handleClick("right")}
+        />
       </div>
-
-      <FiArrowRight
-        className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
-        onClick={() => handleClick('right')}
-      />
     </div>
-  </div>
-  )
+  );
 };
 
 export default MovieRow;
