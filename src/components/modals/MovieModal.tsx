dd82727085d12059d3bfee577fc625177ff5890e
Peterdinis <peterdinis0@pobox.sk>
2023-08-29 14:46:50 +0200

    FOO

--- src/components/modals/MovieModal.tsx
+++ src/components/modals/MovieModal.tsx
@@ -1,11 +1,20 @@
-import {FC} from "react";
+import { FC, useState } from "react";
+import MuiModal from "@mui/material/Modal";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { modalState } from "@/store/atoms/modalAtom";
 
 const MovieModal: FC = () => {
+  const [showModal, setShdowModal] = useRecoilState(modalState);
+
+  const handleClose = () => {
+    setShdowModal(false);
+  };
+
   return (
-    <>
-    
-    </>
-  )
-}
+    <MuiModal open={showModal} onClose={handleClose}>
+      <>Modal</>
+    </MuiModal>
+  );
+};
 
 export default MovieModal;
