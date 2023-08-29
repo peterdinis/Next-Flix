82800d4d82f357d142a6b064114534037d268230
Peterdinis <peterdinis0@pobox.sk>
2023-08-29 15:19:01 +0200

    fix: Fixing bug with movie modal does not get movie data WIP

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -29,6 +29,12 @@ const InfoModal: FC<InfoModalPropsI> = ({
     }
   }, [randomMovie]);
 
+  console.log(movie);
+
+  console.log(currentMovie);
+
+  
+
   return (
     <>
       <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
--- src/components/modals/MovieModal.tsx
+++ src/components/modals/MovieModal.tsx
@@ -3,13 +3,47 @@ import MuiModal from "@mui/material/Modal";
 import { useRecoilState, useRecoilValue } from "recoil";
 import {FiXCircle} from "react-icons/fi";
 import { modalState } from "@/store/atoms/modalAtom";
+import { Genre, Movie, Element } from "@/types/moviesTypes";
 
 const MovieModal: FC = () => {
   const [showModal, setShdowModal] = useRecoilState(modalState);
-
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [trailer, setTrailer] = useState('')
+  const [genres, setGenres] = useState<Genre[]>([])
+  
   const handleClose = () => {
     setShdowModal(false);
   };
+
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_TMDB_API_KEY
+        }&language=en-US&append_to_response=videos`
+      )
+        .then((response) => response.json())
+        .catch((err) => console.log(err.message))
+
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === 'Trailer'
+        )
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+
+    fetchMovie()
+  }, [movie]);
+
+  console.log(movie);
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
