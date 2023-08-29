d6c1715502cf10d8eb5f925a82b3494db8343bb7
Peterdinis <peterdinis0@pobox.sk>
2023-08-29 14:59:36 +0200

    feat: Working on movie modal WIP

--- src/components/modals/MovieModal.tsx
+++ src/components/modals/MovieModal.tsx
@@ -1,6 +1,7 @@
-import { FC, useState } from "react";
+import { FC, Fragment, useState, useEffect } from "react";
 import MuiModal from "@mui/material/Modal";
 import { useRecoilState, useRecoilValue } from "recoil";
+import {FiXCircle} from "react-icons/fi";
 import { modalState } from "@/store/atoms/modalAtom";
 
 const MovieModal: FC = () => {
@@ -12,7 +13,15 @@ const MovieModal: FC = () => {
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
-      <>Modal</>
+      <Fragment>
+        <button className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]" onClick={handleClose}>
+          <FiXCircle className="h-6 w-6 text-blue-50" />
+        </button>
+
+        <div>
+
+        </div>
+      </Fragment>
     </MuiModal>
   );
 };
