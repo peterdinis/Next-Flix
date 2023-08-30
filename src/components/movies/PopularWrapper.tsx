92be49af8d413293c6b655e0eae6fa844963c8e6
Peterdinis <peterdinis0@pobox.sk>
2023-08-30 20:45:18 +0200

    FOO

--- src/components/movies/PopularWrapper.tsx
+++ src/components/movies/PopularWrapper.tsx
@@ -1,19 +1,51 @@
-import { FC } from "react";
+"use client"
 
+import { FC, useState, useRef, useEffect } from "react";
 import SecondNavbar from "../shared/navbar/SecondNavbar";
-import { useFetchNetflixOriginals } from "@/api/movies/moviesRequests";
-import MovieRow from "./MovieRow";
+import useSWR from "swr";
+import { API_KEY, BASE_URL, fetcher } from "@/constants/applictionConstants";
 
 const PopularWrapper: FC = () => {
-  const { data: originalData } = useFetchNetflixOriginals();
+  const [pageIndex, setPageIndex] = useState(1);
+  const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${pageIndex}`;
 
-  console.log(originalData);
+  const { data, error } = useSWR(url, fetcher);
+
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleObserver: IntersectionObserverCallback = ([entry]) => {
+    if (entry.isIntersecting && data && data.page < data.total_pages) {
+      setPageIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [data]);
+
+  if (error) return <div>failed to load</div>;
+
+  console.log(data);
 
   return (
     <>
       <SecondNavbar />
       <section className="md:space-y-24 mt-4">
-        <MovieRow title="Netflix series" movies={originalData} />
+          Popular
       </section>
     </>
   );
