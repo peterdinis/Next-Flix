c55c6ce388515647ff6d329e3652c8efb80ca292
Peterdinis <peterdinis0@pobox.sk>
2023-08-30 20:48:11 +0200

    feat: Adding custom components for error and loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-import ErrorBoundary from "@/components/shared/GlobalError";
+import ErrorBoundary from "@/components/shared/CustomErrorBoundary";
 import AuthContextProvider from "@/context/AuthContext";
 import { RecoilRoot } from "recoil";
 
