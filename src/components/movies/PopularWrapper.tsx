039f5bc54369bb21e5d5d157b6fc1108c1931cdc
Peterdinis <peterdinis0@pobox.sk>
2023-08-31 11:44:58 +0200

    refactor: From swr to react-query

--- src/components/movies/PopularWrapper.tsx
+++ src/components/movies/PopularWrapper.tsx
@@ -1,46 +1,15 @@
 "use client"
 
-import { FC, useState, useRef, useEffect } from "react";
+import { FC, useState} from "react";
 import SecondNavbar from "../shared/navbar/SecondNavbar";
-import useSWR from "swr";
-import { API_KEY, BASE_URL, fetcher } from "@/constants/applictionConstants";
+import { usePaginatedPopular } from "@/api/movies/moviesRequests";
 
 const PopularWrapper: FC = () => {
   const [pageIndex, setPageIndex] = useState(1);
-  const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${pageIndex}`;
 
-  const { data, error } = useSWR(url, fetcher);
-
-  const containerRef = useRef<HTMLDivElement | null>(null);
-
-  const handleObserver: IntersectionObserverCallback = ([entry]) => {
-    if (entry.isIntersecting && data && data.page < data.total_pages) {
-      setPageIndex((prevIndex) => prevIndex + 1);
-    }
-  };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleObserver, {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0,
-    });
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    return () => {
-      if (containerRef.current) {
-        observer.unobserve(containerRef.current);
-      }
-    };
-  }, [data]);
-
-  if (error) return <div>failed to load</div>;
+  const {data} = usePaginatedPopular(pageIndex);
 
   console.log(data);
-
   return (
     <>
       <SecondNavbar />
