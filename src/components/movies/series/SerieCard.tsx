23e7e86db7bb3647506ad967131e18040d674007
Peterdinis <peterdinis0@pobox.sk>
2023-09-01 14:20:50 +0200

    feat: Series card design

--- src/components/movies/series/SerieCard.tsx
+++ src/components/movies/series/SerieCard.tsx
@@ -1,50 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface ISerieProps {
-    title: string;
-    description: string;
+  backdrop_path: string;
+  poster_path: string;
+  name: string;
 }
 
-const SerieCard: FC<ISerieProps> = ({title, description}: ISerieProps) => {
+const SerieCard: FC<ISerieProps> = ({
+  poster_path,
+  backdrop_path,
+  name,
+}: ISerieProps) => {
   return (
-    <div className="p-4 max-w-sm">
-      <div className="flex rounded-lg h-full bg-teal-400 p-8 flex-col">
-        <div className="flex items-center mb-3">
-          <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-          </div>
-          <h2 className="text-white text-lg font-medium">{title}</h2>
-        </div>
-        <div className="flex flex-col justify-between flex-grow">
-          <p className="leading-relaxed text-base text-white">{description}</p>
-          <a href="#" className="mt-3 text-black hover:text-blue-600 inline-flex items-center">
-            Learn More
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition-transform duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${backdrop_path || poster_path}`}
+        className="rounded-sm object-cover md:rounded"
+        alt={name}
+      />
     </div>
   );
-}
+};
 
 export default SerieCard;
--- src/components/movies/series/SeriesWrapper.tsx
+++ src/components/movies/series/SeriesWrapper.tsx
@@ -3,33 +3,32 @@
 import { FC, useState } from "react";
 import SecondNavbar from "../../shared/navbar/SecondNavbar";
 import { usePaginatedSeries } from "@/api/movies/moviesRequests";
-import FeatureCard from "./SerieCard";
+import SerieCard from "./SerieCard";
 import Header from "@/components/shared/Header";
+import { Series } from "@/types/moviesTypes";
 
 const SeriesWrapper: FC = () => {
   const [pageIndex, setPageIndex] = useState(1);
 
   const { data } = usePaginatedSeries(pageIndex);
 
-  console.log(data);
-
   return (
     <>
       <SecondNavbar />
       <Header title="Netflix Series" />
       <div className="flex flex-wrap justify-center mt-10">
-        <FeatureCard
-          title="Feature 1"
-          description="Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine."
-        />
-        <FeatureCard
-          title="Feature 2"
-          description="Lorem ipsum dolor sit amet. In quos laboriosam non neque eveniet nihil molestias. Rem perspiciatis iure ut laborum inventore et maxime amet."
-        />
-        <FeatureCard
-          title="Feature 3"
-          description="Lorem ipsum dolor sit amet. In quos laboriosam non neque eveniet nihil molestias. Rem perspiciatis iure ut laborum inventore et maxime amet."
-        />
+        {data.results &&
+          data.results.map((item: Series) => {
+            return (
+              <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+                <SerieCard
+                  name={item.name}
+                  poster_path={item.poster_path}
+                  backdrop_path={item.backdrop_path}
+                />
+              </div>
+            );
+          })}
       </div>
     </>
   );
