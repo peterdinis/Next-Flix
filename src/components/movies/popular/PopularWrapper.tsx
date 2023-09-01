abfe3a2f16ff2d87ce43d7314445f7f5e9c6190c
Peterdinis <peterdinis0@pobox.sk>
2023-09-01 19:32:57 +0200

    feat: Cleanup

--- src/components/movies/popular/PopularWrapper.tsx
+++ src/components/movies/popular/PopularWrapper.tsx
@@ -4,7 +4,6 @@ import { FC, useState } from "react";
 import SecondNavbar from "../../shared/navbar/SecondNavbar";
 import { usePaginatedPopular } from "@/api/queries/movies/moviesRequests";
 import { Popular } from "@/types/moviesTypes";
-import PopularCard from "./PopularCard";
 import ScrollToTop from "react-scroll-to-top";
 import SecondHeader from "@/components/shared/SecondHeader";
 
@@ -23,11 +22,15 @@ const PopularWrapper: FC = () => {
           data.results.map((item: Popular) => {
             return (
               <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
-                <PopularCard
-                  title={item.title}
-                  poster_path={item.poster_path}
-                  backdrop_path={item.backdrop_path}
-                />
+                <div className="relative h-28 min-w-[180px] cursor-pointer transition-transform duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${
+                      item.backdrop_path || item.poster_path
+                    }`}
+                    className="rounded-sm object-cover md:rounded"
+                    alt={item.title}
+                  />
+                </div>
               </div>
             );
           })}
--- src/components/movies/series/SeriesWrapper.tsx
+++ src/components/movies/series/SeriesWrapper.tsx
@@ -3,8 +3,6 @@
 import { FC, useState } from "react";
 import SecondNavbar from "../../shared/navbar/SecondNavbar";
 import { usePaginatedSeries } from "@/api/queries/movies/moviesRequests";
-import SerieCard from "./SerieCard";
-import Header from "@/components/shared/Header";
 import { Series } from "@/types/moviesTypes";
 import ScrollToTop from "react-scroll-to-top";
 import SecondHeader from "@/components/shared/SecondHeader";
@@ -24,11 +22,15 @@ const SeriesWrapper: FC = () => {
           data.results.map((item: Series) => {
             return (
               <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
-                <SerieCard
-                  name={item.name}
-                  poster_path={item.poster_path}
-                  backdrop_path={item.backdrop_path}
-                />
+                <div className="relative h-28 min-w-[180px] cursor-pointer transition-transform duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${
+                      item.backdrop_path || item.poster_path
+                    }`}
+                    className="rounded-sm object-cover md:rounded"
+                    alt={item.name}
+                  />
+                </div>
               </div>
             );
           })}
