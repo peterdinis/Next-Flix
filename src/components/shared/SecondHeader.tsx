d8416c346d9d54a428a64a8d8af2df40420a3f07
Peterdinis <peterdinis0@pobox.sk>
2023-09-02 10:32:19 +0200

    style: Adding one class to secondHeader component

--- src/components/shared/SecondHeader.tsx
+++ src/components/shared/SecondHeader.tsx
@@ -6,7 +6,7 @@ interface ISecondHeaderProps {
 
 const SecondHeader: FC<ISecondHeaderProps> = ({ title }: ISecondHeaderProps) => {
   return (
-    <h1 className="text-center text-2xl cursor-pointer text-[#e5e5e5] transition duration-200 hover:text-white">
+    <h1 className="text-center text-2xl cursor-pointer text-[#e5e5e5] transition duration-200 font-bold hover:text-white">
       {title}
     </h1>
   );
