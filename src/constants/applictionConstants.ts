966d5bf7b89ffacbe15c4b4ac6018a102397fbff
Peterdinis <peterdinis0@pobox.sk>
2023-09-02 10:22:11 +0200

    feat: Addding pagination to series page

--- src/constants/applictionConstants.ts
+++ src/constants/applictionConstants.ts
@@ -2,6 +2,7 @@ export const TOP_OFFSET: number = 66;
 export const BASE_URL: string = "https://api.themoviedb.org/3";
 export const baseUrl: string = "https://image.tmdb.org/t/p/original";
 export const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY as unknown as string;
+export const TOTAL_SERIES_PAGE: number = 7729;
 
 export const initialMovieData = {
     name: "Custom Name",
