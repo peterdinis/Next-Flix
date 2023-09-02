c37848d3410ee4297187fec602365496aeae9421
Peterdinis <peterdinis0@pobox.sk>
2023-09-02 17:03:18 +0200

    FOO

---
+++ src/components/movies/index.ts
@@ -0,0 +1,9 @@
+import dynamic from "next/dynamic";
+
+export const FilmsWrapper = dynamic(() => import("./films/FilmsWrapper"));
+export const ListWrapper = dynamic(() => import("./myList/ListWrapper"));
+export const PopularWrapper = dynamic(() => import("./popular/PopularWrapper"));
+export const SearchFilms = dynamic(() => import("./search/SearchFilms"));
+export const SearchPopular = dynamic(() => import("./search/SearchPopular"));
+export const SearchSeries = dynamic(() => import("./search/SearchSeries"));
+export const SeriesWrapper = dynamic(() => import("./series/SeriesWrapper"));
