c5f741d18f270953fa687acacec72ccc66d8ee60
Peterdinis <peterdinis0@pobox.sk>
2023-09-14 20:21:01 +0200

    feat: Start getting my films in watch list WIP

--- src/components/movies/myList/ListWrapper.tsx
+++ src/components/movies/myList/ListWrapper.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import SecondNavbar from "../../shared/navbar/SecondNavbar";
 import SecondHeader from "@/components/shared/SecondHeader";
 import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
+import { db } from "@/firebase";
+import { Movie } from "@/types/moviesTypes";
+import { collection } from "firebase/firestore";
 
 const ListWrapper: FC = () => {
+  const [myFilms, setMyFilms] = useState<Movie[] | null>(null);
+
+  const myFilmsRef = collection(db, "customers", "myList");
+
   return (
     <>
       <SecondNavbar />
