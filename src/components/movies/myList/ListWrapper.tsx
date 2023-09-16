aee6dd6f654539faca9a5618123b891396b88c27
Peterdinis <peterdinis0@pobox.sk>
2023-09-16 20:52:57 +0200

    FOO

--- src/components/movies/myList/ListWrapper.tsx
+++ src/components/movies/myList/ListWrapper.tsx
@@ -3,19 +3,30 @@
 import { FC, useState, useEffect } from "react";
 import SecondNavbar from "../../shared/navbar/SecondNavbar";
 import SecondHeader from "@/components/shared/SecondHeader";
-import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
-import { db } from "@/firebase";
-import { Movie } from "@/types/moviesTypes";
-import { collection } from "firebase/firestore";
 import NoFilms from "./NoFilms";
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { db } from "@/firebase";
 
 const ListWrapper: FC = () => {
+  const [myMoviesInList, setMyMoviesInList] = useState<any>();
+
+  useEffect(() => {
+    const q = query(collection(db, "customers"));
+    onSnapshot(q, (querySnapshot) => {
+      setMyMoviesInList(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
   return (
     <>
       <SecondNavbar />
       <SecondHeader title="My Movie List" />
-      <NoFilms />
+      {myMoviesInList.length === 0 ? <NoFilms /> : <>SOME CONTENT</>}
     </>
   );
 };
